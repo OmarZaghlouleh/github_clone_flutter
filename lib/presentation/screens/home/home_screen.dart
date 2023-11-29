065fcafa27242730d21195be7490e002c0acb9b6
Fadi Asfour <fadi.asfour77@gmail.com>
2023-11-29 15:43:43 +0300

    get Groups

---
+++ lib/presentation/screens/home/home_screen.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:github_clone_flutter/core/utils/constants.dart';
+import 'package:github_clone_flutter/core/utils/extensions/media_query.dart';
+import '../../../core/utils/global.dart';
+import '../../style/app_colors.dart';
+import '../profile/profile_screen.dart';
+import 'widgets/home_drawer_component.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Timer.run(() => scaffoldkey.currentState?.openDrawer());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldkey,
+      drawerScrimColor: AppColors.thirdColor.withOpacity(0),
+      drawer: (1.mqWdith(context) < bigScreen) ? const HomeDrawer() : null,
+      body: Container(
+        child: Row(
+          children: [
+            if (1.mqWdith(context) >= bigScreen) const HomeDrawer(),
+            const Expanded(child: ProfileScreen(profileId: -1)),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/screens/home/widgets/home_drawer_component.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:github_clone_flutter/core/utils/extensions/space.dart';
+import 'package:github_clone_flutter/presentation/screens/auth/auth_screen.dart';
+import 'package:github_clone_flutter/presentation/style/app_colors.dart';
+import 'package:github_clone_flutter/presentation/style/app_text_style.dart';
+
+import '../../../../core/utils/app_router.dart';
+import '../../../../core/utils/strings_manager.dart';
+import '../../../../data/data_resource/local_resource/shared_preferences.dart';
+import '../../../common_widgets/divider.dart';
+
+class HomeDrawer extends StatelessWidget {
+  const HomeDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Drawer(
+        // clipBehavior: Clip.hardEdge,
+        // shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(5)),
+        // backgroundColor: Colors.yellow,
+        // shadowColor: Colors.green,
+        // surfaceTintColor: Colors.purple,
+        // elevation: 0,
+        child: Center(
+          child: Column(
+            //crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //Welcome
+              30.space(),
+              // Selector<HomeProvider, HomeDataEntity?>(
+              //   selector: (p0, p1) => p1.getHomeData,
+              //   builder: (context, value, child) => value == null
+              //       ? const EmptyWidget()
+              //       : Padding(
+              //           padding: const EdgeInsets.all(8.0),
+              //           child: Text(
+              //             "${StringManager.welcome} ${value.userName}",
+              //             style: getSmallBoldStyle(
+              //                 color:  AppColors.primaryColor, fontSize: 20),
+              //           ),
+              //         ),
+              // ),
+              15.space(),
+              const CustomDivider(),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ListTile(
+                        onTap: () async {
+                          // Navigator.pushNamed(
+                          //     context, RoutesManager.beneficiariesListRoute);
+                        },
+                        title: Text(
+                          "My groups",
+                          style: AppTextStyle.headerTextStyle(),
+                        ),
+                        leading: const Icon(
+                          Icons.people_rounded,
+                          color: AppColors.primaryColor,
+                        ),
+                      ),
+                      const CustomDivider(),
+                      ExpansionTile(
+                        title: Text(
+                          StringManager.logout,
+                          style: AppTextStyle.getSmallBoldStyle(
+                            color: AppColors.errorColor,
+                          ),
+                        ),
+                        leading: const Icon(
+                          Icons.logout,
+                          color: AppColors.errorColor,
+                        ),
+                        children: [
+                          ListTile(
+                            onTap: () async {
+                              //TODO: add API request
+                              LocalResource.deleteUserData();
+                              AppRouter.navigateReplacementTo(
+                                  context: context,
+                                  destination: const AuthScreen());
+                            },
+                            title: Text(
+                              StringManager.logoutFromThisDevice,
+                              style: AppTextStyle.getSmallBoldStyle(
+                                color: AppColors.secondaryColor,
+                              ),
+                            ),
+                            leading: const Icon(
+                              Icons.stop_screen_share_outlined,
+                              color: AppColors.secondaryColor,
+                            ),
+                          ),
+                          ListTile(
+                            onTap: () async {
+                              //TODO: add API request
+                              LocalResource.deleteUserData();
+                              AppRouter.navigateReplacementTo(
+                                  context: context,
+                                  destination: const AuthScreen());
+                            },
+                            title: Text(
+                              StringManager.logoutFromAllDevices,
+                              style: AppTextStyle.getSmallBoldStyle(
+                                color: AppColors.secondaryColor,
+                              ),
+                            ),
+                            leading: const Icon(
+                              Icons.devices_other_outlined,
+                              color: AppColors.secondaryColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                      50.space(),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
